9b488457bf7212887956663da9669ba4ef609c39
KnDmitriy <99825353+KnDmitriy@users.noreply.github.com>
2023-10-26 09:48:22 +0400

    Add files via upload

---
+++ Three Layer/ThreeLayer.Common/DependensyResolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreeLayer.BLL;
+using ThreeLayer.DAL;
+
+namespace ThreeLayer.Common
+{
+    public static class DependencyResolver
+    {
+        static private ICatalog catalog_;
+        static private IDiskLogic diskLogic;
+        static public ICatalog Catalog_
+        {
+            get => catalog_ ?? (catalog_ = new Catalog());
+        }
+        static public IDiskLogic DiskLogic_
+        {
+            get => diskLogic ?? (diskLogic = new DiskLogic(Catalog_));
+        }
+    }
+}
